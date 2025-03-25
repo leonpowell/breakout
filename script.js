dc6e4adc262c3259014bb5b31fc9d411ecf7edc3
ian <leonpowell2013@gmail.com>
2025-03-25 06:58:21 +0000

    add ball bounce off wall

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
-const ballRadius = 10;
+let dx = 2;
+let dy = -2;
+
+
+
 
 
 
@@ -17,12 +20,20 @@ ctx.closePath();
 }
 
 function draw() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-drawBall();
-x += dx;
-y += dy;
-}
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+  
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+      dy = -dy;
+    }
+  
+    x += dx;
+    y += dy;
+  }
+  
 function startGame() {
 setInterval(draw, 10);
 }
