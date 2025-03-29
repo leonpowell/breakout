f095ae89f8fd66c2bf308497ef96cc5ba97e3b8a
ian <leonpowell2013@gmail.com>
2025-03-29 12:26:16 +0000

    update dimensions of board and added js for bricks

--- script.js
+++ script.js
@@ -12,6 +12,39 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth)/2
 let interval = 0;
 
+//brick variables
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+
+for(let c= 0; c<brickColumnCount; c++){
+    bricks[c]=[];
+    for(let r= 0; r<brickRowCount; r++){
+        bricks[c][r] = {x:0, y:0};
+    }
+}
+
+function drawBricks(){
+    for(let c= 0; c<brickColumnCount; c++){
+        for(let r= 0; r<brickRowCount; r++){
+            const brickX = c*(brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = r*(brickHeight + brickPadding) + brickOffsetTop;
+            bricks[c][r].x = 0;
+            bricks[c][r].x = 0;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle= "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 
 
@@ -37,6 +70,7 @@ function draw() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 drawBall();
 drawPaddle();
+drawBricks();
 
 if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
